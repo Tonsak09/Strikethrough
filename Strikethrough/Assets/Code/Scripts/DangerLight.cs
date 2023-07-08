d9fac81e7af20ac9f64f732d68a8d531bb1ab6c1
Narai Risser (RIT Student) <ngr7989@rit.edu>
2023-07-08 00:05:44 -0400

    Shader & art

--- Strikethrough/Assets/Code/Scripts/DangerLight.cs
+++ Strikethrough/Assets/Code/Scripts/DangerLight.cs
@@ -4,8 +4,17 @@ using UnityEngine;
 
 public class DangerLight : MonoBehaviour
 {
+    [SerializeField] float timeTillDie = 1.0f;
+    private float count;
+
     private void OnTriggerStay(Collider other)
     {
-        
+        count += Time.deltaTime;
+
+        if(count > timeTillDie)
+        {
+            // Restart to closest respace location 
+            print("Restart");
+        }
     }
 }
